87db9bf3fc88d41757e05f8df20ca734aebdd4f0
mediadocena <45419684+mediadocena@users.noreply.github.com>
2020-06-07 00:20:08 +0200

    const

--- src/app/Const/const.ts
+++ src/app/Const/const.ts
@@ -1,2 +1,2 @@
-//export const api = 'https://flaskproyectofinal.herokuapp.com/';
-export const api = 'http://127.0.0.1:5000/';
+export const api = 'https://flaskproyectofinal.herokuapp.com/';
+//export const api = 'http://127.0.0.1:5000/';
